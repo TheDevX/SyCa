2ba46ff6de0573f4b11d00341e9368300dca88ea
TheDevX <61651402+TheDevX@users.noreply.github.com>
2020-03-01 12:12:28 +0100

    Update pdoconecction.php

--- meta/sql/pdoconecction.php
+++ meta/sql/pdoconecction.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=db1268.mydbserver.com;dbname=usr_p397345_5', 'p397345', '5PDgmzoV');
+$pdo = new PDO('mysql:host=CENSORED;dbname=CENSORED', 'CENSORED', 'CENSORED');
 ?>
